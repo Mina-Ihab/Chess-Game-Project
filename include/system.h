355d57163d72768ae9111d408b536ad9a8dccd57
Mina-Ihab <minaihab762@gmail.com>
2025-12-25 22:21:45 +0200

    Edit headers

--- include/system.h
+++ include/system.h
@@ -3,10 +3,11 @@
 
 #include <wchar.h>
 
+extern int saveSlot, max_slot;
 void start(wchar_t **board);
 void main_menu(wchar_t **board);
-void undo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* max, int* Wdead, int*Bdead, int update);
+void undo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* Wdead, int*Bdead, int update);
 int save_move(wchar_t*** memory_board, wchar_t** board, int* Wdead, int* Bdead);
-void redo_move(wchar_t*** memory_board, wchar_t** board, int max, int* error, int* Wdead, int* Bdead);
+void redo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* Wdead, int* Bdead);
 
 #endif
