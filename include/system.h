9b4546d082a60cae220870defc562bc563c326ee
Mina-Ihab <minaihab762@gmail.com>
2025-12-25 12:13:29 +0200

    redo function added

--- include/system.h
+++ include/system.h
@@ -7,5 +7,6 @@ void start(wchar_t **board);
 void main_menu(wchar_t **board);
 void undo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* max, int* Wdead, int*Bdead, int update);
 int save_move(wchar_t*** memory_board, wchar_t** board, int* Wdead, int* Bdead);
+void redo_move(wchar_t*** memory_board, wchar_t** board, int max, int* error, int* Wdead, int* Bdead);
 
 #endif
