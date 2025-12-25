a670189d0d214ca8d2d48d005118d7d6d11d59c3
Mina-Ihab <minaihab762@gmail.com>
2025-12-25 08:22:39 +0200

    update headers

--- include/system.h
+++ include/system.h
@@ -5,7 +5,7 @@
 
 void start(wchar_t **board);
 void main_menu(wchar_t **board);
-void undo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* max, int update, int* setting_array);
-int save_move(wchar_t*** memory_board, wchar_t** board);
+void undo_move(wchar_t*** memory_board, wchar_t** board, int* error, int* max, int* Wdead, int*Bdead, int update);
+int save_move(wchar_t*** memory_board, wchar_t** board, int* Wdead, int* Bdead);
 
 #endif
